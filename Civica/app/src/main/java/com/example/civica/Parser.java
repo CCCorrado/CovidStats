f9e89008a3b55939559f4fac74c5270a9be5aa8e
Diego Sardelli <sardellidiego20@gmail.com>
2022-05-13 12:17:50 +0200

    Aggiunto ptogetto android studio

---
+++ Civica/app/src/main/java/com/example/civica/Parser.java
@@ -0,0 +1,17 @@
+
+package com.example.civica;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class Parser {
+
+    public String parseUrl(int year, int month, int)
+
+
+    public static void main(String[] args) throws IOException {
+    }
+}
