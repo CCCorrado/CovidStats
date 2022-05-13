f231ff40ec54a524176eb68991c86066e492adfa
Corrado Cionini <corrado.cionini@studenti.galileilivorno.edu.it>
2022-05-13 11:32:01 +0200

    Added parser file

---
+++ Parser.java
@@ -0,0 +1,15 @@
+package EdCivicaTPSI;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class Parser {
+
+    public String parseUrl(int year, int month, int )
+
+    public static void main(String[] args) throws IOException {
+    }
+}
