4419073ce0e2d1ae70087e9bc565f7552d5e0393
Corrado Cionini <corrado.cionini@studenti.galileilivorno.edu.it>
2022-05-15 14:55:35 +0200

    Parser probably finished

--- Civica/app/src/main/java/com/example/civica/Parser.java
+++ Civica/app/src/main/java/com/example/civica/Parser.java
@@ -1,4 +1,3 @@
-
 package com.example.civica;
 
 import java.io.BufferedReader;
@@ -8,10 +7,46 @@ import java.io.InputStreamReader;
 import java.net.*;
 
 public class Parser {
+    public Parser(){}
 
-    public String parseUrl(int year, int month, int)
+    public String parseUrl(int year, int month, int day){
+        BufferedReader buffer = null;
+        StringBuilder sb = null;
+        String mese = String.format("%02d", month);  // => "01"
+        String giorno = String.format("%02d", day);
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-" + year + mese + giorno + ".csv");
+            URLConnection connection = url.openConnection();
 
+            InputStreamReader input = new InputStreamReader(connection.getInputStream());
+            String line = "";
+            String csvSplitBy = ",";
+
+            sb = new StringBuilder();
+            buffer = new BufferedReader(input);
+            while ((line = buffer.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            return sb.toString();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (buffer != null) {
+                try {
+                    buffer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
 
     public static void main(String[] args) throws IOException {
+        Parser p = new Parser();
+        System.out.println(p.parseUrl(2020, 10, 1));
     }
 }
--- Parser.java
+++ Parser.java
@@ -5,15 +5,30 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.HashMap;
 
 public class Parser {
-    public Parser(){}
+    private HashMap<String, Integer> mapGiorno;
+    private HashMap<String, Integer> mapProvincia;
 
-    public String parseUrl(int year, int month, int day){
+    public Parser(){
+        mapGiorno = new HashMap<>();
+        mapProvincia = new HashMap<>();
+    }
+
+    public HashMap<String,Integer> getMapGiorno() {
+        return this.mapGiorno;
+    }
+
+    public HashMap<String,Integer> getMapProvincia() {
+        return this.mapProvincia;
+    }
+
+    public boolean parseGiorno(int year, int month, int day){
         BufferedReader buffer = null;
-        StringBuilder sb = null;
         String mese = String.format("%02d", month);  // => "01"
         String giorno = String.format("%02d", day);
+        mapGiorno.clear();
         try {
             URL url = new URL("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-" + year + mese + giorno + ".csv");
             URLConnection connection = url.openConnection();
@@ -22,13 +37,16 @@ public class Parser {
             String line = "";
             String csvSplitBy = ",";
         
-            sb = new StringBuilder();
             buffer = new BufferedReader(input);
             while ((line = buffer.readLine()) != null) {
-                sb.append(line + "\n");
+                String row[] = line.split(csvSplitBy);
+                if(row[0].equals("data") || row[5].equals("In fase di definizione/aggiornamento")){
+                    continue;
+                }
+                mapGiorno.put(row[5], Integer.parseInt(row[9]));
             }
-            return sb.toString();
-
+            
+            return true;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -42,11 +60,50 @@ public class Parser {
                 }
             }
         }
-        return null;
+        return false;
+    }
+
+    public boolean parseProvincia (String provincia) {
+        BufferedReader buffer = null;
+        mapProvincia.clear();
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv");
+            URLConnection connection = url.openConnection();
+
+            InputStreamReader input = new InputStreamReader(connection.getInputStream());
+            String line = "";
+            String csvSplitBy = ",";
+        
+            buffer = new BufferedReader(input);
+            while ((line = buffer.readLine()) != null) {
+                String row[] = line.split(csvSplitBy);
+                if(row[5].equals(provincia)){
+                    String data[] = row[0].split("T");
+                    mapProvincia.put(data[0], Integer.parseInt(row[9]));
+                }
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (buffer != null) {
+                try {
+                    buffer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) throws IOException {
         Parser p = new Parser();
-        System.out.println(p.parseUrl(2020, 10, 1));
+        if(p.parseProvincia("Matera")){
+            HashMap<String, Integer> map = p.getMapProvincia();
+            System.out.println(map.get("2022-01-01"));
+        }
     }
 }
