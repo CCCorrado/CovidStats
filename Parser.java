755b4635b9412c30bd4e65667c5af34e0daa54f2
Corrado Cionini <corrado.cionini@studenti.galileilivorno.edu.it>
2022-05-15 15:36:59 +0200

    Added parser download

--- Parser.java
+++ Parser.java
@@ -10,10 +10,12 @@ import java.util.HashMap;
 public class Parser {
     private HashMap<String, Integer> mapGiorno;
     private HashMap<String, Integer> mapProvincia;
+    private HashMap<String, Integer> mapDownload;
 
     public Parser(){
         mapGiorno = new HashMap<>();
         mapProvincia = new HashMap<>();
+        mapDownload = new HashMap<>();
     }
 
     public HashMap<String,Integer> getMapGiorno() {
@@ -22,6 +24,10 @@ public class Parser {
 
     public HashMap<String,Integer> getMapProvincia() {
         return this.mapProvincia;
+    }
+
+    public HashMap<String,Integer> getMapDownload() {
+        return this.mapDownload;
     }
 
     public boolean parseGiorno(int year, int month, int day){
@@ -99,11 +105,79 @@ public class Parser {
         return false;
     }
 
+    public String getLast(){
+        BufferedReader buffer = null;
+        mapProvincia.clear();
+        try {
+            // Si ottiene nella variabile last l'ultimo giorno del dataset
+            URL url = new URL("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-latest.csv");
+            URLConnection connection = url.openConnection();
+            InputStreamReader input = new InputStreamReader(connection.getInputStream());
+            buffer = new BufferedReader(input);
+
+            buffer.readLine();
+            String line = buffer.readLine();
+            String last = line.split(",")[0].split("T")[0];
+            return last;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (buffer != null) {
+                try {
+                    buffer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean parseDownload(){
+        BufferedReader buffer = null;
+        mapDownload.clear();
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv");
+            URLConnection connection = url.openConnection();
+
+            InputStreamReader input = new InputStreamReader(connection.getInputStream());
+            String line = "";
+            String csvSplitBy = ",";
+            
+            buffer = new BufferedReader(input);
+            while ((line = buffer.readLine()) != null) {
+                String row[] = line.split(csvSplitBy);
+                if(row[0].equals("data") || row[5].equals("In fase di definizione/aggiornamento") || row[5].equals("Fuori Regione / Provincia Autonoma")){
+                    continue;
+                }
+                String key = row[5] + "D" + row[0].split("T")[0];
+                //System.out.println(key);
+                mapDownload.put(key, Integer.parseInt(row[9]));
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (buffer != null) {
+                try {
+                    buffer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         Parser p = new Parser();
-        if(p.parseProvincia("Matera")){
-            HashMap<String, Integer> map = p.getMapProvincia();
-            System.out.println(map.get("2022-01-01"));
-        }
+        System.out.println(p.parseDownload());
+        HashMap<String, Integer> map = p.getMapDownload();
+        String key = "LivornoD2022-05-05";
+        System.out.println(map.get(key));
     }
 }
