cc8691561d7de5e5419ca693335c19d72c1204f8
Corrado Cionini <corrado.cionini@studenti.galileilivorno.edu.it>
2022-05-16 17:04:54 +0200

    Loading screen

--- prova/app/src/main/java/com/example/civica/MainActivity.java
+++ prova/app/src/main/java/com/example/civica/MainActivity.java
@@ -3,26 +3,35 @@ package com.example.civica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Parser p = new Parser();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        RelativeLayout r = findViewById(R.id.loading);
+        r.setVisibility(View.VISIBLE);
         Thread thread = new Thread(new Runnable() {
 
             @Override
             public void run() {
                 try  {
-                    System.out.println("lol");
-                    Parser p = new Parser();
                     p.parseDownload();
                     HashMap map = p.infoGiorno("2020-08-06");
                     System.out.println(map);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            r.setVisibility(View.GONE);
+                        }
+                    });
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
